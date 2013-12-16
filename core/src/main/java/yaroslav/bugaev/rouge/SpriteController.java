073889e75f4db8e6b466d3af25659ecd86d3668e
Yaroslav Bugaev <scourgegames@gmail.com>
2013-12-16 02:05:08 +0200

    Enhanced movement control

--- core/src/main/java/yaroslav/bugaev/rouge/SpriteController.java
+++ core/src/main/java/yaroslav/bugaev/rouge/SpriteController.java
@@ -1,5 +1,6 @@
 package yaroslav.bugaev.rouge;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -8,15 +9,31 @@ import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Timer;
 
 public class SpriteController extends InputAdapter {
     private Sprite sprite;
     private MapObjects collisionMap;
     private final float offset = 24;
+    private final Timer.Task moveTask;
 
     public SpriteController(Sprite sprite, MapObjects collisionMap) {
         this.sprite = sprite;
         this.collisionMap = collisionMap;
+        moveTask = new Timer.Task() {
+            @Override
+            public void run() {
+                if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+                    keyDown(Input.Keys.LEFT);
+                } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+                    keyDown(Input.Keys.RIGHT);
+                } else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+                    keyDown(Input.Keys.UP);
+                } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+                    keyDown(Input.Keys.DOWN);
+                }
+            }
+        };
     }
 
     @Override
@@ -32,6 +49,16 @@ public class SpriteController extends InputAdapter {
             vector.set(0, -offset);
         }
 
+        move(vector);
+        if (moveTask.isScheduled()) {
+            moveTask.cancel();
+        }
+        Timer.schedule(moveTask, 0.2f);
+
+        return super.keyDown(keycode);
+    }
+
+    private void move(Vector2 vector) {
         Rectangle nextPosition = sprite.getBoundingRectangle();
         nextPosition.setHeight(1);
         Vector2 nextPositionVector = new Vector2();
@@ -40,13 +67,13 @@ public class SpriteController extends InputAdapter {
         for (MapObject mapObject : collisionMap) {
             if (mapObject.getProperties().get("type", String.class).equals("Collision")) {
                 if (nextPosition.overlaps(((RectangleMapObject) mapObject).getRectangle())) {
-                    return false;
+                    return;
                 }
             }
         }
 
         sprite.setPosition(nextPosition.getX(), nextPosition.getY());
-
-        return super.keyDown(keycode);
     }
+
+
 }
